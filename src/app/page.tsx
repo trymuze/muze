7a5b048c6eee4121ad23459b9a98cfdef0214a15
pranjalanimesh <pranjalanimesh5@gmail.com>
2024-02-22 23:16:35 +0530

    changed design pran

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import RectangleRounded from "../components/rectangle-rounded";
 
 const GalileoDesign: NextPage = () => {
   return (
-    <div className="w-full h-[1863px] relative bg-white flex flex-col items-start justify-start tracking-[normal] mq950:h-auto mq950:min-h-[1862.9]">
-      <main className="w-full my-0 mx-[!important] absolute h-full top-[0px] right-[0px] bottom-[0px] left-[0px] bg-whitesmoke flex flex-col items-center justify-start pt-0 px-0 pb-10 box-border gap-[36px_0px] max-w-full mq700:gap-[36px_0px]">
+    <div className="w-full h-[1863px] relative bg-[#fcf8ba] diagonal-gradient-background flex flex-col items-start justify-start tracking-[normal] mq950:h-auto mq950:min-h-[1862.9]">
+      <main className="w-full my-0 mx-[!important] absolute h-full top-[0px] right-[0px] bottom-[0px] left-[0px] flex flex-col items-center justify-start pt-0 px-0 pb-10 box-border gap-[36px_0px] max-w-full mq700:gap-[36px_0px]">
         <Depth2Frame />
         <section className="w-[1000px] flex flex-col items-center justify-start py-0 px-5 box-border gap-[56px_0px] max-w-full shrink-0 text-left text-[48px] text-white font-plus-jakarta-sans mq700:gap-[56px_0px]">
           <div className="w-[928px] rounded-xl overflow-hidden flex flex-row items-end justify-start pt-4 px-4 pb-0 box-border bg-[url('/Depth-6-Frame-0.png')] bg-cover bg-no-repeat bg-[top] max-w-full">
@@ -15,7 +15,7 @@ const GalileoDesign: NextPage = () => {
                   <div className="self-stretch flex flex-col items-start justify-start gap-[8px_0px] max-w-full">
                     <div className="self-stretch flex flex-col items-start justify-start max-w-full">
                       <div className="self-stretch flex flex-col items-start justify-start max-w-full">
-                        <h1 className="m-0 text-black relative text-inherit tracking-[-1.58px] leading-[60px] font-extrabold font-inherit inline-block max-w-full mq450:text-10xl mq450:leading-[36px] mq950:text-[38px] mq950:leading-[48px]">
+                        <h1 className="m-0 text-white relative text-inherit tracking-[-1.58px] leading-[60px] font-extrabold font-inherit inline-block max-w-full mq450:text-10xl mq450:leading-[36px] mq950:text-[38px] mq950:leading-[48px]" style={{fontFamily:"Pontiac", fontSize:"70px"}}>
                           Manage your audience like a pro
                         </h1>
                       </div>
--- src/components/depth2-frame.tsx
+++ src/components/depth2-frame.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 
 const Depth2Frame: NextPage = () => {
     return (
-        <header className="self-stretch box-border flex flex-row items-center justify-between py-3 px-10 gap-[0px] top-[0] [row-gap:20px] z-[99] sticky max-w-full text-left text-lg text-gray font-plus-jakarta-sans border-b-[1px] border-solid border-gainsboro bg-orange-400">
+        <header className="self-stretch box-border flex flex-row items-center justify-between py-3 px-10 gap-[0px] top-[0] [row-gap:20px] z-[99] sticky max-w-full text-left text-lg text-gray font-plus-jakarta-sans border-b-[1px] border-solid border-gainsboro bg-[#000]">
             <div className="flex flex-row items-center justify-start">
                 <div className="flex flex-row items-center justify-start gap-[0px_16px]">
                     <div className="flex flex-col items-start justify-start">
@@ -12,8 +13,15 @@ const Depth2Frame: NextPage = () => {
                     </div>
                     <div className="h-[22px] flex flex-col items-start justify-start">
                         <div className="flex flex-col items-start justify-start">
-                            <b className="relative tracking-[-0.27px] leading-[22.5px] whitespace-nowrap">
-                                Muze
+                            <b className="relative tracking-[-0.27px] leading-[22.5px] whitespace-nowrap text-[#ff810a]">
+                                {/* <p className="-mt-0" style={{ fontFamily: 'Pontiac', fontSize:"40px" }}>Muze</p> */}
+                                <Image 
+                                    src="/muze3.png"
+                                    alt="Muze"
+                                    width={160}
+                                    height={98}
+                                    className="m-0 p-0 -mt-9"
+                                />
                             </b>
                         </div>
                     </div>
@@ -48,10 +56,10 @@ const Depth2Frame: NextPage = () => {
                 </div>
                 <div className="self-stretch w-44 flex flex-row items-start justify-start gap-[0px_8px]">
 
-                    <div className="self-stretch rounded-xl bg-aliceblue overflow-hidden flex flex-row items-center justify-center py-0 px-4">
-                        <div className="bg-aliceblue overflow-hidden flex flex-col items-start justify-start">
+                    <div className="self-stretch rounded-xl bg-[#FF7B00] overflow-hidden flex flex-row items-center justify-center py-0 px-4">
+                        <div className="bg-[#FF7B00] overflow-hidden flex flex-col items-start justify-start">
                             <div className="flex flex-col items-start justify-start">
-                                <b className="relative tracking-[0.21px] leading-[21px] whitespace-nowrap">
+                                <b className="relative tracking-[0.21px] leading-[21px] whitespace-nowrap text-black" style={{fontFamily:"Pontiac", fontSize:"16px"}}>
                                     Sign up
                                 </b>
                             </div>
